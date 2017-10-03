494e364b2a6b8e128d54f805235a1bc93e6c96b1
Emi Chen <emimbchen@gmail.com>
2017-10-03 11:21:32 -0500

    base mode complete

--- client.js
+++ client.js
@@ -8,5 +8,49 @@ var mayella = { name: "Mayella", employeeNumber: "89068", annualSalary: "35000",
 var employees = [ atticus, jem, boo, scout, robert, mayella ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+var Employee = function( employeeIn ){
+  this.name = employeeIn.name;
+  this.bonusPercentage = bonusCalc(employeeIn);
+  this.totalBonus = parseInt(employeeIn.annualSalary) * this.bonusPercentage;
+  this.totalCompensation = parseInt(employeeIn.annualSalary) + this.totalBonus;
+};
 
+function bonusCalc(employee){
+  var bonus=0;
+  switch(employee.reviewRating){
+    case 3:
+    bonus = 0.04;
+    break;
+    case 4:
+    bonus = 0.06;
+    break;
+    case 5:
+    bonus = 0.10;
+    break;
+    default:
+    bonus = 0;
+}
+if(employee.employeeNumber.length===4){
+  bonus+= 0.05;
+}
+if(parseInt(employee.annualSalary)>65000){
+  bonus-=0.01;
+  }
+if(bonus<0){
+  bonus=0;
+}
+else if(bonus>0.13){
+  bonus=0.13;
+}
+return bonus;
+}
 console.log(employees);
+
+for (var i = 0; i < employees.length; i++){
+    console.log(employeeBonus(employees[i]));
+}
+
+function employeeBonus(employeeIn){
+  var bonusEmployee = new Employee(employeeIn);
+  return bonusEmployee;
+}
